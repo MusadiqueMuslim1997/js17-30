18f24c69b858359274a2957743dbf2fda82bcde2
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2023-12-31 23:11:43 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+
+
+// chapter 17-20
+
+
+// Question: 1 
+// var array = [];
+
+
+// Question: 2
+
+// var num = [0,1,2,3,1,0,1,2,2,1,0,1];
+
+// Question: 3
+
+// for(i = 1; i <= 10; i++){
+//     document.write(i+"<br />")
+// }
+
+
+// Question: 4
+
+// tableNum = Number (prompt("Enter any table number",5));
+// tableLength = Number (prompt("Enter table length you want",10))
+
+// for(i = 1; i <= tableLength; i++){
+//     document.write(tableNum + " x " + i + " = " + tableNum*i + "<br />")
+// }
+
+
+// Question: 5
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for(i = 0; i < fruits.length; i ++){
+//     document.write( fruits[i] + "<br />"+  i + "<br />")
+// }
+
+
+
+// Question: 6
+
+// a
+
+// for(i = 1; i <= 15; i++){
+//     document.write(i)
+// }
+
+
+// b 
+// for(i = 10; i >= 1; i--){
+//     document.write(i)
+// }
+
+
+// c
+
+// for(i = 0; i <= 20; i++){
+//     if(i%2 === 0){
+//         document.write(i + "<br />")
+
+//     }
+// }
+
+
+// for(i = 0; i <= 20; i++){
+//     if(i%2 !== 0){
+//         document.write(i + "<br />")
+
+//     }
+// }
+
+// for(i = 2; i <= 20; i++){
+//     if(i%2 === 0){
+//         document.write(i + "k" +  "<br />")
+
+//     }
+// }
